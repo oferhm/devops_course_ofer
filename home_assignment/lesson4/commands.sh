61f17f69ac1795728126e251755161491dd51c90
Ofer Haim <ofer.hm@gmail.com>
2025-09-11 17:16:26 +0300

    Add commands to apply Kubernetes configurations

---
+++ home_assignment/lesson4/commands.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://github.com/oferhm/devops_course_ofer/tree/oferhm-patch-1/home_assignment/lesson4/pc.yaml
+kubectl apply -f https://github.com/oferhm/devops_course_ofer/tree/oferhm-patch-1/home_assignment/lesson4/pvc.yaml
