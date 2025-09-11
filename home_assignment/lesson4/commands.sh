a54844db994a44532945311013a8b4439e1b248c
Ofer Haim <ofer.hm@gmail.com>
2025-09-11 17:21:46 +0300

    Update kubectl commands to use raw URLs

--- home_assignment/lesson4/commands.sh
+++ home_assignment/lesson4/commands.sh
@@ -1,2 +1,2 @@
-kubectl apply -f https://github.com/oferhm/devops_course_ofer/tree/oferhm-patch-1/home_assignment/lesson4/pc.yaml
-kubectl apply -f https://github.com/oferhm/devops_course_ofer/tree/oferhm-patch-1/home_assignment/lesson4/pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/pv.yaml
+kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/pvc.yaml
