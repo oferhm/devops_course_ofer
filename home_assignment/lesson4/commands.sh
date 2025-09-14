1c146ff649d3d7254821b8e2690d731372e21e49
Ofer Haim <ofer.hm@gmail.com>
2025-09-14 13:33:13 +0300

    Add commands to apply ConfigMap and Deployment

--- home_assignment/lesson4/commands.sh
+++ home_assignment/lesson4/commands.sh
@@ -1,2 +1,4 @@
 kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/pv.yaml
 kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/configMap.yaml
+kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/deployment.yaml
