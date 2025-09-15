722060a15e948aa5fb7e068a240ea53edfb6af7b
Ofer Haim <ofer.hm@gmail.com>
2025-09-15 13:46:57 +0300

    Add cronjob.yaml application command

--- home_assignment/lesson4/commands.sh
+++ home_assignment/lesson4/commands.sh
@@ -2,3 +2,5 @@ kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/ref
 kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/pvc.yaml
 kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/configMap.yaml
 kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/deployment.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/oferhm/devops_course_ofer/refs/heads/oferhm-patch-1/home_assignment/lesson4/cronjob.yaml
